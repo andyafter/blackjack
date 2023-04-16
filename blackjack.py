a84b36a5fcee5150c8339c72eb82d23b43b8607e
AndyPan <panan@hanyasushis-MacBook-Pro.local>
2023-04-16 19:59:55 +0800

    snake case

--- blackjack.py
+++ blackjack.py
@@ -319,39 +319,38 @@ class Round:
     
 if __name__ == "__main__":
 
-    ROUNDS=1
-    HARD_STRATEGY, SOFT_STRATEGY, PAIR_STRATEGY=StrategyImporter('./strategies/BasicStrategy.csv').import_player_strategy()
-    basicStrategy=[HARD_STRATEGY, SOFT_STRATEGY, PAIR_STRATEGY]
+    ROUNDS = 1
+    HARD_STRATEGY, SOFT_STRATEGY, PAIR_STRATEGY = StrategyImporter('./strategies/BasicStrategy.csv').import_player_strategy()
+    basic_strategy = [HARD_STRATEGY, SOFT_STRATEGY, PAIR_STRATEGY]
     countings = []
     nb_hands = 0
-    roundList=[]
-    betList=[]
-    pnlList=[]
-    totalBet=0
-    totalPnL=0
-    sh=Shuffler(6,{"max":4,"weight":{1:1,2:1,3:1,4:1}})
+    round_list = []
+    bet_list = []
+    pnl_list = []
+    total_bet = 0
+    total_pnl = 0
+    sh = Shuffler(6, {"max": 4, "weight": {1: 1, 2: 1, 3: 1, 4: 1}})
 
     for r in range(ROUNDS):
-        round = Round(1,sh,basicStrategy) #TODO: vary bet size according to True Count
+        round = Round(1, sh, basic_strategy)  # TODO: vary bet size according to True Count
         round.playRound()
         for i in range(round.player_number):
             for hand in round.players[i].hands:
                 nb_hands += 1
                 win, bet = round.get_hand_winnings(hand)
-                round.totalBet += bet
-                round.totalWin += win
-        roundList.append(round)
-        betList.append(round.totalBet)
-        pnlList.append(round.totalWin)
-        totalBet+=round.totalBet
-        totalPnL+=round.totalWin
+                round.total_bet += bet
+                round.total_win += win
+        round_list.append(round)
+        bet_list.append(round.total_bet)
+        pnl_list.append(round.total_win)
+        total_bet += round.total_bet
+        total_pnl += round.total_win
         sh.shuffle_back()
-        
-    
-    print("Bet List",betList)
-    print("pnl List",pnlList)
-    print("Total Bet = ", totalBet)
-    print("Total PnL = ", totalPnL)
+
+    print("Bet List", bet_list)
+    print("pnl List", pnl_list)
+    print("Total Bet = ", total_bet)
+    print("Total PnL = ", total_pnl)
         
         #countings += game.shoe.count_history
 
