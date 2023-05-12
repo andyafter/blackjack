fa387b23202d0340052d6128c1305115612f21e0
AndyPan <andyafter@gmail.com>
2023-05-12 16:42:21 +0800

    for loop shell script

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
-
-python run_hands.py 0 &
-python run_hands.py 1 &
-python run_hands.py 2 &
-python run_hands.py 3 &
-python run_hands.py 4 &
-python run_hands.py 5 &
-python run_hands.py 6 &
-python run_hands.py 7 &
-python run_hands.py 8 &
-python run_hands.py 9 &
+for i in {0..9}
+do
+   python run_hands.py $i &
+done
 
 # wait for all background processes to finish
 wait
